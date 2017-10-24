540fd6ae69a4ae306be8d8caa34fb3282892b369
Eric Hameleers <alien@slackware.com>
2017-10-24 15:52:07 +0200

    Add some hints for the future in the KDE profile scripts
    Using /run/user instead of /tmp for XDG_RUNTIME_DIR would be more in line
    with XDG specs, but in that case we should mount /run as tmpfs and
    add this to the Slackware rc scripts after mounting /run:
      mkdir /run/user
      chmod 1777 /run/user
    
    Then, the XDG_RUNTIME_DIR definition would become:
      XDG_RUNTIME_DIR=/run/user/$USER

--- kde/post-install/kservice/profile.d/kde.csh
+++ kde/post-install/kservice/profile.d/kde.csh
@@ -12,6 +12,10 @@ else
 endif
 
 if ( ! $?XDG_RUNTIME_DIR ) then
+    # Using /run/user would be more in line with XDG specs, but in that case
+    # we should mount /run as tmpfs and add this to the Slackware rc scripts:
+    # mkdir /run/user ; chmod 1777 /run/user
+    # setenv XDG_RUNTIME_DIR /run/user/$USER
     setenv XDG_RUNTIME_DIR /tmp/xdg-runtime-$USER
     mkdir -p $XDG_RUNTIME_DIR
     chown $USER $XDG_RUNTIME_DIR
--- kde/post-install/kservice/profile.d/kde.sh
+++ kde/post-install/kservice/profile.d/kde.sh
@@ -10,6 +10,10 @@ else
   XDG_CONFIG_DIRS=/etc/xdg:/etc/kde/xdg
 fi
 if [ "$XDG_RUNTIME_DIR" = "" ]; then
+  # Using /run/user would be more in line with XDG specs, but in that case
+  # we should mount /run as tmpfs and add this to the Slackware rc scripts:
+  # mkdir /run/user ; chmod 1777 /run/user
+  # XDG_RUNTIME_DIR=/run/user/$USER
   XDG_RUNTIME_DIR=/tmp/xdg-runtime-$USER
   mkdir -p $XDG_RUNTIME_DIR
   chown $USER $XDG_RUNTIME_DIR
